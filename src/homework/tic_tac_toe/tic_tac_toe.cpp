5c5b69a0fb363b90331b0a797be0430d3eca6cbf
KrisCplusplus <kristin.clement1@g.austincc.edu>
2019-10-09 15:39:58 -0500

    Tic Tac Toe hw .5

--- src/homework/tic_tac_toe/tic_tac_toe.cpp
+++ src/homework/tic_tac_toe/tic_tac_toe.cpp
@@ -1,1 +1,54 @@
 //cpp
+#include "tic_tac_toe.h"
+
+bool TicTacToe::game_over()
+{
+	return false;
+}
+
+void TicTacToe::start_game(string player)
+{
+	next_player = player;
+	clear_board();
+}
+
+void TicTacToe::mark_board(int position)
+{
+}
+
+void TicTacToe::set_next_player()
+{
+	if (next_player == "X")
+	{
+		next_player = "O";
+	}
+	else
+	{
+		next_player = "X";
+	}
+}
+
+bool TicTacToe::check_column_win()
+{
+
+}
+
+bool TicTacToe::check_row_win()
+{
+
+}
+
+bool TicTacToe::check_diagonal_win()
+{
+
+}
+
+void TicTacToe::clear_board()
+{
+
+}
+
+bool TicTacToe::check_board_full()
+{
+
+}
--- src/homework/tic_tac_toe/tic_tac_toe.h
+++ src/homework/tic_tac_toe/tic_tac_toe.h
@@ -1,1 +1,26 @@
 //h
+#include<string>
+#include<vector>
+
+using std::string; using std::vector;
+/*
+Represents one game of tic tac toe.
+*/
+class TicTacToe 
+{
+public:
+	bool game_over();
+	void start_game(string player);
+	void mark_board(int position);
+
+
+	private: 
+		void set_next_player();
+		bool check_column_win();
+		bool check_row_win();
+		bool check_diagonal_win();
+		void clear_board();
+		bool check_board_full();
+		vector<string> pegs(9, " ");
+		string next_player;
+};
