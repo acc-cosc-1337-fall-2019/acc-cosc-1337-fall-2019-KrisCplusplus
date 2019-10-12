adefedc0de7ede0e33da6d554846ed41c8291d07
Art Gonzalez <arturo.gonzalez@austincc.edu>
2019-10-11 23:58:26 -0500

    Fix formatting

--- src/homework/tic_tac_toe/tic_tac_toe.h
+++ src/homework/tic_tac_toe/tic_tac_toe.h
@@ -16,13 +16,13 @@ public:
 	void display_board() const;
 
 
-	private: 
-		void set_next_player();
-		bool check_column_win();
-		bool check_row_win();
-		bool check_diagonal_win();
-		void clear_board();
-		bool check_board_full();
-		vector<string> pegs(9, " ");//initalizer
-		string next_player;
+private: 
+	void set_next_player();
+	bool check_column_win();
+	bool check_row_win();
+	bool check_diagonal_win();
+	void clear_board();
+	bool check_board_full();
+	vector<string> pegs(9, " ");//initalizer
+	string next_player;
 };
